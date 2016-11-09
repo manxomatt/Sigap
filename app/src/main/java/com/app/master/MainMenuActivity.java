2d8fe5ba8c17388e7915fd6f5198c99e7c5f9e49
Parama Dharmika <parama.dharmika@gmail.com>
2016-11-10 02:30:24 +0700

    Fixes: remove top right menu

--- app/src/main/java/com/app/master/MainMenuActivity.java
+++ app/src/main/java/com/app/master/MainMenuActivity.java
@@ -133,13 +133,13 @@ public class MainMenuActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        setFontNavigationOpen();
-        return true;
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.main_menu, menu);
+//        setFontNavigationOpen();
+//        return true;
+//    }
 
     /*
     @Override
